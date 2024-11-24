7aab53851f2099d2e65e91197c3289a0d4c4adeb
Nedas Barsteika <100798234+NedasBarsteika@users.noreply.github.com>
2024-11-24 18:37:12 +0200

    added main as a page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,9 @@
-import steps from './images/steps.png'
-import logo from './images/skillit.png'
+import Main from './pages/Main.jsx'
 
 export default function App() {
     return (
         <>
-            <div className='App'>
-                <img className="steps" src={steps} />
-                <img className="logo" src={logo} />
-                <h2>COMPETENCE</h2>
-                <h2>ASSESSMENT</h2>
-                <h2>FORM</h2>
-                <button>SSO <span>&gt;</span> </button>
-            </div>
+            <Main />
         </>
     )
 }
